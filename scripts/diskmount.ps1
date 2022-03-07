a253fb019d5f0e46a51aeafaf68045ddf3fe37bc
acloud56 <35495677+acloud56@users.noreply.github.com>
2022-03-07 14:10:31 -0500

    Update diskmount.ps1

--- scripts/diskmount.ps1
+++ scripts/diskmount.ps1
@@ -1,3 +1,21 @@
+$initial_d_drive = Get-CimInstance Win32_LogicalDisk -Filter 'DeviceID="D:"' | Select-Object -Unique
+if ($initial_d_drive.DriveType -eq 5) 
+{
+    Write-Output 'Drive D is a CD-ROM, moving it to F:'
+    $drv = Get-WmiObject win32_volume -filter 'DriveLetter = "D:"'
+    $drv.DriveLetter = "F:"
+    $drv.Put()
+}
+
+$initial_e_drive = Get-CimInstance Win32_LogicalDisk -Filter 'DeviceID="E:"' | Select-Object -Unique
+if ($initial_e_drive.DriveType -eq 5) 
+{
+    Write-Output 'Drive E is a CD-ROM, moving it to G:'
+    $drv = Get-WmiObject win32_volume -filter 'DriveLetter = "E:"'
+    $drv.DriveLetter = "G:"
+    $drv.Put()
+}
+
 $mounts = "D,P"
 $label = "Data,PageFile"
 ### Stops the Hardware Detection Service ###
